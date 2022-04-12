23ab2696da5f8656212fdffd5f1a9f04357a3c1e
maryam <socialmari3@gmail.com>
2022-04-12 11:47:03 +0300

    Fix: Buttons

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,39 +1,58 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import WalletForm from "../WalletForm/WalletForm";
 import "./Navbar.css";
-import coins from './coins.png'
-import {Link } from 'react-router-dom';
+import coins from "./coins.png";
+import { Link } from "react-router-dom";
 import { ListWalletContext } from "../../Context/ListWalletContext";
+import db from "../../Firebase";
+import { useAuth } from "../../Context/AuthContext";
 
 export default function NavBar() {
   const [modal, setModal] = useState(false);
+  const [userIn, setUserIn] = useState("/sign-up");
   const { WalletList } = useContext(ListWalletContext);
+  const { currentUser } = useAuth();
+  useEffect(() => {
+    currentUser ? setUserIn("/profile") : setUserIn("/sign-up");
+  }, [currentUser]);
   return (
     <div className="navBar">
       <div>
         <div className="justify-content-between">
-          <div className='coins-logo-brand'> 
-          {WalletList.length === 0 ? <Link to='/Nowallets/' style={{ color: 'white'}}> <h2 className='wallet-name'> B-Planner </h2> </Link> : <Link to='/wallets/' style={{ color: 'white'}}> <h2> B-Planner </h2> </Link> }
-          <img src={coins} alt='coins logo' className='coins-logo'/>
+          <div className="coins-logo-brand">
+            {WalletList.length === 0 ? (
+              <Link to="/Nowallets/" style={{ color: "white" }}>
+                {" "}
+                <h2 className="wallet-name"> B-Planner </h2>{" "}
+              </Link>
+            ) : (
+              <Link to="/wallets/" style={{ color: "white" }}>
+                {" "}
+                <h2> B-Planner </h2>{" "}
+              </Link>
+            )}
+            <img src={coins} alt="coins logo" className="coins-logo" />
           </div>
-         <div>
-          <button
-            className="btn-color addwallet-fixed"
-            onClick={() => setModal(true)}
+          <div>
+            <button
+              className="btn-color addwallet-fixed"
+              onClick={() => setModal(true)}
+              disabled={currentUser ? false : true}
             >
-            Add wallet
-          </button>
-          <Link to='/sign-up'> 
-          <button
-            className="btn-color btn-prof "
-            >
-            Profile
-          </button>
-          </Link>
-        </div> 
+              Add wallet
+            </button>
+
+            <Link to={userIn}>
+              <button className="btn-color btn-prof ">Profile</button>
+            </Link>
+          </div>
         </div>
       </div>
-      <WalletForm show={modal} setShow={setModal} onHide={() => setModal(false)} />
+      <WalletForm
+        show={modal}
+        setShow={setModal}
+        onHide={() => setModal(false)}
+      />
     </div>
   );
 }
--- src/Pages/NoWalletPage/NoWalletPage.js
+++ src/Pages/NoWalletPage/NoWalletPage.js
@@ -1,29 +1,48 @@
 import React, { useState } from "react";
 import WalletForm from "../../Components/WalletForm/WalletForm";
-import wal from './wallet_aym5.png'
+import wal from "./wallet_aym5.png";
 import "./NoWalletPage.css";
+import { useAuth } from "../../Context/AuthContext";
+import { useHistory } from "react-router-dom";
 
 export default function NoWallet() {
   const [modal, setModal] = useState(false);
+  const { currentUser } = useAuth();
+  let history = useHistory();
 
   return (
     <div className="nomain">
       <div className="nomain-parent-div">
-          <div className="u-hv-no-wllt"> 
-            <div className='wlc-sp'> Welcome to </div><div className='wlc-sp2'> Budget Planner </div>
-            <h3 className='crt-head'> Take control of your money and save them by tracking your expenses.</h3>
-            <button
-              type="submit"
-              onClick={() => setModal(true)}
-              className="add-wallet-btn-nomain"
-            >
-               Create Wallet
-            </button>
-          </div>
-         
-          <div className='wallet-illus-cont'> <img src={wal} alt='wallet illustration'className='wallet-illus'/> </div>
+        <div className="u-hv-no-wllt">
+          <div className="wlc-sp"> Welcome to </div>
+          <div className="wlc-sp2"> Budget Planner </div>
+          <h3 className="crt-head">
+            {" "}
+            Take control of your money and save them by tracking your expenses.
+          </h3>
+          <button
+            type="submit"
+            onClick={() => currentUser ? setModal(true) : history.push('/sign-up') }
+            className="add-wallet-btn-nomain"
+          >
+            Create Wallet
+          </button>
+        </div>
 
-        <WalletForm show={modal} setShow={setModal} onHide={() => setModal(false)} />
+        <div className="wallet-illus-cont">
+          {" "}
+          <img
+            src={wal}
+            alt="wallet illustration"
+            className="wallet-illus"
+          />{" "}
+        </div>
+
+        <WalletForm
+          show={modal}
+          setShow={setModal}
+          onHide={() => setModal(false)}
+        />
       </div>
     </div>
   );
